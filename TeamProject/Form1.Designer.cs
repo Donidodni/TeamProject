ad5d95438b49b44f317fbce0ec7b081d61abc32e
우열 이 <20182392@vision.hoseo.edu>
2024-01-30 15:19:17 +0900

    업데이트(이우열)

--- TeamProject/Form1.Designer.cs
+++ TeamProject/Form1.Designer.cs
@@ -46,9 +46,9 @@
             label2.AutoSize = true;
             label2.Location = new System.Drawing.Point(300, 189);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(39, 15);
+            label2.Size = new System.Drawing.Size(51, 15);
             label2.TabIndex = 0;
-            label2.Text = "label1";
+            label2.Text = "asdfasdf";
             // 
             // Form1
             // 
