dfb55707e4ef3707e69cc04e570b75ab781c654d
SEOK7 <68433260+Donidodni@users.noreply.github.com>
2024-01-31 16:35:19 +0900

    Add files via upload
    (김민석) 메인화면 업로드

--- TeamProject/Form1.Designer.cs
+++ TeamProject/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TeamProject
+﻿namespace WinFormsApp2
 {
     partial class Form1
     {
@@ -28,36 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Main_Title = new System.Windows.Forms.Timer(components);
+            pictureBox2 = new System.Windows.Forms.PictureBox();
+            imageList1 = new System.Windows.Forms.ImageList(components);
+            panel1 = new System.Windows.Forms.Panel();
+            panel2 = new System.Windows.Forms.Panel();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            button1 = new System.Windows.Forms.Button();
+            pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
-            // label1
+            // Main_Title
             // 
-            //label1.AutoSize = true;
-            //label1.Location = new System.Drawing.Point(138, 189);
-            //label1.Name = "label1";
-            //label1.Size = new System.Drawing.Size(39, 15);
-            //label1.TabIndex = 0;
-            //label1.Text = "label1";
+            Main_Title.Interval = 18;
+            Main_Title.Tick += Main_Title_Tick;
             // 
-            // label2
+            // pictureBox2
             // 
-            //label2.AutoSize = true;
-            //label2.Location = new System.Drawing.Point(300, 189);
-            //label2.Name = "label2";
-            //label2.Size = new System.Drawing.Size(39, 15);
-            //label2.TabIndex = 0;
-            //label2.Text = "label1";
+            pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            pictureBox2.Image = (System.Drawing.Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new System.Drawing.Point(334, 172);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(646, 218);
+            pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (System.Windows.Forms.ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            imageList1.Images.SetKeyName(0, "opening1.gif");
+            imageList1.Images.SetKeyName(1, "opening_sword.png");
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            panel1.Location = new System.Drawing.Point(0, 498);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(784, 63);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            panel2.Location = new System.Drawing.Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(784, 56);
+            panel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (System.Drawing.Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new System.Drawing.Point(12, 161);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(115, 240);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.Color.IndianRed;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button1.Font = new System.Drawing.Font("맑은 고딕", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button1.ForeColor = System.Drawing.Color.Transparent;
+            button1.Location = new System.Drawing.Point(224, 413);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(331, 64);
+            button1.TabIndex = 6;
+            button1.Text = "시작하기";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (System.Drawing.Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new System.Drawing.Point(83, 161);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new System.Drawing.Size(115, 240);
+            pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            pictureBox3.Visible = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
+            BackColor = System.Drawing.SystemColors.ControlLight;
+            BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            ClientSize = new System.Drawing.Size(784, 561);
+            Controls.Add(button1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox2);
+            DoubleBuffered = true;
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+        private System.Windows.Forms.Timer Main_Title;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
--- TeamProject/Form1.cs
+++ TeamProject/Form1.cs
@@ -7,15 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;  //배경 음악 사용을 위해 필요하며, 프로젝트 - 프로젝트 참조 추가 - COM - Media.player 를 선택하여 활성화 합니다.
 
-namespace TeamProject
+namespace WinFormsApp2
 {
     public partial class Form1 : Form
     {
+
+        static int second_count = 0; // 그라데이션 전용 카운트 변수입니다.
+        Timer fade = new Timer();
+
         public Form1()
         {
-            InitializeComponent(); // 1/30 푸시 테스트 2
+            InitializeComponent();
+            Main_Title.Interval = 18;
+            Main_Title.Enabled = true;
+            pictureBox3.Visible = false;
+            playSimpleSound(1); // 폼 시작과 함께 음악을 재생합니다.
         }
 
+
+
+        private void playSimpleSound(int play) // 매개변수 있는 음악 재생 메서드 입니다. 
+        {
+  
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\c#\WinFormsApp2\Properties\project_bgm.wav");// 배경 음악 경로(주의 : wav 파일만 인식하며, mp3 사용시 인식 불가 에러 뜸)          
+            if (play == 1) simpleSound.Play(); // 매개변수를 사용하여 음악 재생을 컨트롤 합니다. 
+            else simpleSound.Stop();
+        }
+        private void playDingSound(int play)
+        {
+            SoundPlayer DingSound = new SoundPlayer(@"C:\c#\WinFormsApp2\Properties\bing_sound.wav");
+            if (play == 1) DingSound.Play(); // 매개변수를 사용하여 음악 재생을 컨트롤 합니다. 
+            else DingSound.Stop();
+        }
+
+
+
+
+        private void Main_Title_Tick(object sender, EventArgs e)
+        {
+            second_count++; // 그라데이션 전용 카운트 변수입니다.
+
+            double gradient = 10 * Math.Pow(0.9, second_count); // 용사 사진에 그라데이션 이펙트를 부여합니다
+            Point point = new Point(pictureBox1.Location.X + (int)gradient, pictureBox1.Location.Y);
+            pictureBox1.Location = point;
+
+
+            double gradient1 = 22 * Math.Pow(0.9, second_count); // 제목 사진에 그라데이션 이펙트를 부여합니다.
+            Point point1 = new Point(pictureBox2.Location.X - (int)gradient1, pictureBox2.Location.Y);
+            pictureBox2.Location = point1;
+
+            if (second_count == 40) // 두 사진의 그라데이션 이펙트 부여 길이를 조절합니다. second_count가 40 이면 중지 및 리셋합니다. 
+            {
+                Main_Title.Enabled = false;
+                pictureBox3.Visible = true;
+                playDingSound(1);
+                second_count = 0;
+            }   
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            playSimpleSound(0); // 음악을 종료합니다.
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Opacity = 0;
+            fade.Interval = 10;
+            fade.Tick += new EventHandler(fadeIn);
+            fade.Start();
+        }
+        void fadeIn(object sender, EventArgs e)
+        {
+            if (Opacity >= 1)
+                fade.Stop();
+            else
+                Opacity += 0.02;
+        }
     }
 }
